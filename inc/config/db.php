42c12d17e298a5ab5951f0c696de0d57986ccd04
Kshitija-2001 <bharambekshitija2@gmail.com>
2022-04-22 12:26:18 +0530

    2 commit

---
+++ inc/config/db.php
@@ -0,0 +1,23 @@
+<?php
+
+$host = 'mysql-76575-0.cloudclusters.net';
+$db   = 'shop_inventory';
+$user = 'admin';
+$pass = 'jIvoJ9hR';
+$port = "10738";
+$charset = 'utf8mb4';
+
+$options = [
+    \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+    \PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
+try {
+     $pdo = new \PDO($dsn, $user, $pass, $options);
+     echo "Connected successfully";
+} catch (\PDOException $e) {
+     echo "Connection failed: " . $e->getMessage();
+}
+
+?>
